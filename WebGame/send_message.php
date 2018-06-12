5746ddd1717af11ecb4a82370d83068bfcb453f9
fei.long <fei.long@msxf.com>
2018-06-12 21:03:57 +0800

    [练习]

--- WebGame/send_message.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "./functions.php";
-include "./dbconnect.php";
-if($_GET[message])
-$_GET[message] = $_GET[message];
-mysql_query("update `room` set `message_".$_GET[site]."` = '".$_GET[message]."' where `ID` = '".$_GET[roomid]."'");
-?>
-
